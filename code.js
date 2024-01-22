d2e77c55e0c16715db10a518fb6d076a29435dfc
Muhammad Saad <saadmizri434@gmail.com>
2024-01-22 11:11:16 +0500

    ui update

--- code.js
+++ code.js
@@ -2,14 +2,16 @@ document.getElementById('thumbnail').addEventListener('click', function() {
     document.getElementById('overlayyyy').style.display = 'flex';
     document.getElementById('thumbnail').style.display = 'none';
     document.getElementById('name').style.display = 'none';
-    document.getElementById('social-media').style.display = 'none';
+    document.getElementById('projrcts').style.display = 'none';
+    document.getElementById('info-and-social').style.display = 'none';
   });
   
   document.getElementById('closeBtnnnn').addEventListener('click', function() {
       document.getElementById('overlayyyy').style.display = 'none';
       document.getElementById('thumbnail').style.display = 'block';
       document.getElementById('name').style.display = 'block';
-      document.getElementById('social-media').style.display = 'flex';
+      document.getElementById('projrcts').style.display = 'block';
+      document.getElementById('info-and-social').style.display = 'flex';
   });
 
   document.getElementById('overlayyyy').addEventListener('click', function(event) {
@@ -17,6 +19,7 @@ document.getElementById('thumbnail').addEventListener('click', function() {
       document.getElementById('overlayyyy').style.display = 'none';
       document.getElementById('thumbnail').style.display = 'block';
       document.getElementById('name').style.display = 'block';
-      document.getElementById('social-media').style.display = 'flex';
+      document.getElementById('projrcts').style.display = 'block';
+      document.getElementById('info-and-social').style.display = 'flex';
     }
   });
